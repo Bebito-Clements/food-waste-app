545f210c9d08b333cdfef1ae1ca6b056e985f4c8
Bebito-Clements <itsmebebito31@gmail.com>
2025-06-03 03:00:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
 # update for streamlit
+import streamlit as st
+import pandas as pd
+import sqlite3
+
+st.set_page_config(page_title="Food App", layout="wide")
+st.title("🍱 Local Food Wastage Management System")
+st.write("✅ App loaded successfully!")
